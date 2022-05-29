2d615b798718a0f1c73929026aaaf276c23817be
MR. XENZ <102963507+Xenz-11@users.noreply.github.com>
2022-05-29 20:33:30 +0700

    Create clone.py

---
+++ clone.py
@@ -0,0 +1,14 @@
+import os,sys
+try:
+        import requests
+except ImportError:
+        os.system('pip install requests')
+try:
+        import rich
+except ImportError:
+        os.system('pip install rich')
+try:
+        import bs4
+except ImportError:
+        os.system('pip install bs4')
+os.system('python clone.cython-easy-38.so')
